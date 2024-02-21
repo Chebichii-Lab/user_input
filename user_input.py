7238d1d61e1e1144dea68d2a14e9e94f0080963d
Chebichii-Lab <chebichiiserem@gmail.com>
2024-02-21 06:44:28 +0300

    initial commit

---
+++ user_input.py
@@ -0,0 +1,10 @@
+name = input("What is your name: ")
+print (name)
+
+age = input("How old are you: ")
+print (age)
+
+location = input("Where do you come from: ")
+print (location)
+
+print (f"Hello {name}, you are {age} years old and you live in {location}")
